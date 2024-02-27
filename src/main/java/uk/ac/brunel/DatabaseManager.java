135d5f16a7f5ed073c4058d088f5f510981d8e48
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-02-27 16:02:34 +0000

    add function to load items from db

--- src/main/java/uk/ac/brunel/DatabaseManager.java
+++ src/main/java/uk/ac/brunel/DatabaseManager.java
@@ -1,5 +1,6 @@
 package uk.ac.brunel;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DatabaseManager {
 
@@ -8,6 +9,29 @@ public class DatabaseManager {
 	public Connection conn() throws SQLException {
 		Connection conn =  DriverManager.getConnection(url);
 		return conn;
+	}
+	
+	// Fetch all doctors from database and return arraylist of doctor objects  
+	public ArrayList<Doctor> getAllDoctors(Connection conn) throws SQLException {
+		
+		ArrayList<Doctor> allDoctors = new ArrayList<Doctor>();
+		
+		ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM doctors_info");
+		while(rs.next()) {
+			
+			int id = rs.getInt("id");
+			String doctorName = rs.getString("doctor_name");
+			String specialization = rs.getString("specialization");
+			double reviewRating = rs.getDouble("review_rating");
+			int totalReviews = rs.getInt("total_reviews");
+			
+			Doctor doctor = new Doctor(id,doctorName, specialization, reviewRating, totalReviews);
+			allDoctors.add(doctor);
+		}
+		
+		
+		return allDoctors;
+		
 	}
 	
 }
