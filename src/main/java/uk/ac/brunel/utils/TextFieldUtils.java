469e4c7a9af5871b51e11d41ae16217be0ff3f33
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-03-08 15:17:12 +0000

    add class to handle textfield functions

---
+++ src/main/java/uk/ac/brunel/utils/TextFieldUtils.java
@@ -0,0 +1,55 @@
+package uk.ac.brunel.utils;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class TextFieldUtils {
+
+	public enum CharLimit {
+	    NAME(25),
+	    TITLE(25),
+	    TEXT(350);
+
+	    private final int charCount;
+
+	    // Constructor
+	    private CharLimit(int charCount) {
+	        this.charCount = charCount;
+	    }
+
+	    // Method to return display name
+	    public int getCharCount() {
+	        return charCount;
+	    }
+	}
+	
+	//private final static int maxCharacters = 350;
+	
+	public static void limitCharacters(TextArea textArea, CharLimit limit) {
+        textArea.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.length() > limit.getCharCount()) {
+                textArea.setText(oldValue); 
+            }
+            
+            String filteredValue = newValue.replaceAll("[^a-zA-Z0-9 ]", ""); // Allow only letters, digits, and spaces
+            if (!filteredValue.equals(newValue)) {
+                textArea.setText(filteredValue);
+            }
+
+        });
+    }
+	
+	public static void limitTextFieldCharacters(TextField textField, CharLimit limit) {
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.length() > limit.getCharCount()) {
+                textField.setText(oldValue); 
+            }
+            
+            // TODO: allow certain punctuation for title maybe
+            String filteredValue = newValue.replaceAll("[^a-zA-Z0-9 ]", ""); // Allow only letters, digits, and spaces
+            if (!filteredValue.equals(newValue)) {
+                textField.setText(filteredValue);
+            }
+        });
+    }
+	
+}
