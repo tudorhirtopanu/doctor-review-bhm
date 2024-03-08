b3f2cd3be72cc8f41403bc6e9a3d29d2beb33df6
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-03-08 15:17:32 +0000

    fetch reviews to display on ui

--- src/main/java/uk/ac/brunel/controllers/ViewReviewController.java
+++ src/main/java/uk/ac/brunel/controllers/ViewReviewController.java
@@ -1,6 +1,7 @@
 package uk.ac.brunel.controllers;
 
 import java.io.IOException;
+import uk.ac.brunel.managers.*;
 
 import uk.ac.brunel.models.Review;
 import java.net.URL;
@@ -21,18 +22,32 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.ScrollPane;
 import javafx.geometry.Insets;
 
+import java.sql.*;
+
 
 public class ViewReviewController implements Initializable  {
+	
+	private DatabaseManager dbManager = new DatabaseManager();
 	
 	@FXML private ImageView brunelLogo;
     @FXML private Image logoImage = new Image(getClass().getResourceAsStream("/brunel_logo.png"));
     
+    @FXML private ImageView backButtonImg;
+    @FXML private Image backArrow = new Image(getClass().getResourceAsStream("/backArrow.png"));
+    
     @FXML private Button backButton;
     @FXML private Label doctorName;
+    @FXML private Label doctorSpecialisationLabel;
     @FXML private GridPane reviewGrid;
     @FXML private ScrollPane scrollPane;
     
     private ArrayList<Review> reviews;
+    
+    private Doctor doctor;
+    
+    public ViewReviewController(Doctor doctor) {
+        this.doctor = doctor;
+    }
 
     @FXML private void navigateToHome() {
     	try {
@@ -48,10 +63,12 @@ public class ViewReviewController implements Initializable  {
     
 	public void initData(Doctor doctor) {
 		doctorName.setText(doctor.getName());
+		doctorSpecialisationLabel.setText(doctor.getSpecialization());
+		//this.doctor = doctor;
+		//System.out.println("asdfasdf "+ this.doctor.getID());
     }
 	
 	private ArrayList<Review> createDummyReviews() {
-        // Dummy method to create some sample reviews
 		
         ArrayList<Review> dummyReviews = new ArrayList<>();
         dummyReviews.add(new Review(1, 2, "Name 1", "Title 1", "Great service and approved very fast and tablets sent the next day. This was way better than trying and failing to get through to my doctors surgery, by time I'd have got an appointment my ailment would have gone and I needed tablets immediately. Yes, you pay a little more but the convenience far outweighs that. Yes, you pay a little more but the", "Today",1));
@@ -59,18 +76,24 @@ public class ViewReviewController implements Initializable  {
         dummyReviews.add(new Review(3, 2, "Name 3", "Title 3", "Text 3", "Today", 1));
         dummyReviews.add(new Review(4, 2, "Name 4", "Title 4", "Text 4", "Today", 2));
         dummyReviews.add(new Review(5, 2, "Name 5", "Title 5", "Text 5", "Today", 4));
+        
+        try {
+    		dummyReviews.addAll(dbManager.getReviews(doctor.getID()));
+    		//System.out.println("Doctor id is "+doctor.getID());
+    	} catch (SQLException e) {
+    	    e.printStackTrace();
+    	}
+        
         // Add more dummy reviews as needed
         return dummyReviews;
+          
     }
 	
-	@Override
-    public void initialize(URL location, ResourceBundle resources) {
-    	
+	private void setUpReview() {
+		
 		reviews = createDummyReviews();
-    	// Set logo image 
-    	brunelLogo.setImage(logoImage);
-    	
-    	// Add reviews to the GridPane
+		
+		// Add reviews to the GridPane
         int columnIndex = 0;
         int rowIndex = 0;
         
@@ -102,6 +125,22 @@ public class ViewReviewController implements Initializable  {
         scrollPane.setFitToWidth(true);
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setContent(reviewGrid);
+		
+	}
+	
+	
+	@Override
+    public void initialize(URL location, ResourceBundle resources) {
+    	
+    	// Set logo image 
+    	brunelLogo.setImage(logoImage);
+    	
+    	backButtonImg.setImage(backArrow);
+    	
+    	backButtonImg.setFitWidth(25);
+    	backButtonImg.setFitHeight(11);
+    	
+    	setUpReview();
     
     }
 	
